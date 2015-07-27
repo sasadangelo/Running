25130163bce0de48a1b2b0770c087e6d5f2c3ecb
sasadangelo <sal16nic>
2015-07-27 10:50:03 +0200

    Added GPS support with Speed/Peace calculation.

---
+++ app/src/main/java/running/org/running/Constants.java
@@ -0,0 +1,9 @@
+package running.org.running;
+
+/**
+ * Created by Administrator on 7/15/2015.
+ */
+public interface Constants {
+    public static final int INDEX_KMH = 0;
+    public static final int INDEX_MIN_KM = 1;
+}
