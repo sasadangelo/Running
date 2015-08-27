e876e989c19412d7080d2bcab355a14cd200b7c4
sasadangelo <sal16nic>
2015-08-27 15:27:48 +0200

    Integrate Metronome source code.

--- app/src/main/java/running/org/running/Metronome.java
+++ app/src/main/java/running/org/running/Metronome.java
@@ -17,238 +17,202 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Metronome extends ActionBarActivity {
-	boolean mRunning = false;
+	private static final int MAX_TEMPO = 220;
+	private static final int DEFAULT_TEMPO = 75;
+
+	private SeekBar metronomeSeekBar;
+	private Button metronomePlusButton;
+	private Button metronomeMinusButton;
+	private TextView metronomeText;
+
+	private int metronomeTempo = DEFAULT_TEMPO;
+
+	private boolean metronomeActive = false;
+	private boolean metronomeContinue = true;
+
 	Button mStartStopButton;
-	SeekBar mSeekBar;
 	TickPlayer tp;
-	TextView tempoVal;
-	TextView mPeriodLabel;
-	Button mPeriodButtons[];
-	Button mPlus;
-	Button mMinus;
 	PowerManager.WakeLock mWakeLock;
 
-	private static final int DEFAULT_TEMPO = 75;
-	private static final int DEFAULT_PERIOD = 4;
-	private int mTempo = DEFAULT_TEMPO;
-	private int mPeriod = DEFAULT_PERIOD;
-	private static final int numPeriods = 8;
-	private static final int maxTempo = 200;
 	private static final String KEY_TEMPO = "METRONOME_TEMPO";
-	private static final String KEY_PERIOD = "METRONOME_PERIOD";
-	
 	private static final String PREFS = "metronome.prefs";
 
-	private boolean metronomeChecked = false;
+	boolean metronomeRunning = false;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		//Log.v("Metronome", "onCreate");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_metronome);
+
 		PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
 		mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MetronomeLock");
 		tp = new TickPlayer(this);
+
+		metronomeSeekBar = (SeekBar) findViewById(R.id.tempo);
+		metronomeSeekBar.setMax(MAX_TEMPO + 1);
+		metronomeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+				metronomeTempo = progress;
+				metronomeText.setText("" + metronomeTempo);
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				metronomeTempo = seekBar.getProgress();
+				restart();
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+		});
+
+		metronomeMinusButton = (Button) findViewById(R.id.minus);
+		metronomeMinusButton.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (metronomeTempo > 1) --metronomeTempo;
+				restart();
+			}
+		});
+
+		metronomePlusButton = (Button) findViewById(R.id.plus);
+		metronomePlusButton.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (metronomeTempo < MAX_TEMPO) ++metronomeTempo;
+				restart();
+			}
+		});
+
+		metronomeText = (TextView) findViewById(R.id.text);
+
+		CheckBox metronomecheckBox= (CheckBox) findViewById(R.id.checkbox_metronome);
+		metronomecheckBox.setChecked(metronomeActive);
+
+		RadioGroup metronomeRadioGroup= (RadioGroup) findViewById(R.id.radiogroup_metronome);
+		if (metronomeActive) {
+			metronomeRadioGroup.setVisibility(View.VISIBLE);
+			metronomeSeekBar.setVisibility(View.VISIBLE);
+			metronomeMinusButton.setVisibility(View.VISIBLE);
+			metronomePlusButton.setVisibility(View.VISIBLE);
+			metronomeText.setVisibility(View.VISIBLE);
+		} else {
+			metronomeRadioGroup.setVisibility(View.GONE);
+			metronomeSeekBar.setVisibility(View.GONE);
+			metronomeMinusButton.setVisibility(View.GONE);
+			metronomePlusButton.setVisibility(View.GONE);
+			metronomeText.setVisibility(View.GONE);
+		}
+
+		RadioButton metronomeRadioContinue= (RadioButton) findViewById(R.id.radio_metronome_continue);
+		RadioButton metronomeRadioIntervals= (RadioButton) findViewById(R.id.radio_metronome_intervals);
+		if (metronomeContinue) {
+			metronomeRadioContinue.setChecked(true);
+			metronomeRadioIntervals.setChecked(false);
+		} else {
+			metronomeRadioContinue.setChecked(false);
+			metronomeRadioIntervals.setChecked(true);
+		}
+
 		mStartStopButton = (Button) findViewById(R.id.startstop);
-
-		mSeekBar = (SeekBar) findViewById(R.id.tempo);
-		mSeekBar.setMax(maxTempo + 1);
-		tempoVal = (TextView) findViewById(R.id.text);
-		mMinus = (Button) findViewById(R.id.minus);
-		mPlus = (Button) findViewById(R.id.plus);
-		mPeriodLabel = (TextView) findViewById(R.id.period);
-
-		SharedPreferences settings = getSharedPreferences(PREFS, 0);
-		mPeriod = settings.getInt(KEY_PERIOD, DEFAULT_PERIOD);
-		mTempo = settings.getInt(KEY_TEMPO, DEFAULT_TEMPO);
-
-		bindPeriodButtons();
-
-		mMinus.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (mTempo > 1) --mTempo;
-				restart();
-			}
-		});
-		mPlus.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (mTempo < maxTempo) ++mTempo;
-				restart();
-			}
-		});
-
-		mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-												@Override
-												public void onProgressChanged(SeekBar seekBar,
-																			  int progress, boolean fromTouch) {
-													mTempo = progress;
-													tempoVal.setText("" + mTempo);
-													// TODO Auto-generated method stub
-												}
-
-												@Override
-												public void onStopTrackingTouch(SeekBar seekBar) {
-
-													mTempo = seekBar.getProgress();
-													restart();
-													//tp.onStop();
-													//tp.onStart(4, val);
-												}
-
-												@Override
-												public void onStartTrackingTouch(SeekBar seekBar) {
-													// TODO Auto-generated method stub
-												}
-											}
-		);
-
 		mStartStopButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				changeState();
 			}
 		});
+
+		SharedPreferences settings = getSharedPreferences(PREFS, 0);
+		metronomeTempo = settings.getInt(KEY_TEMPO, DEFAULT_TEMPO);
 		restart();
-
-		CheckBox metronomecheckBox= (CheckBox) findViewById(R.id.checkbox_metronome);
-		metronomecheckBox.setChecked(metronomeChecked);
-
-		RadioGroup metronomeRadioGroup= (RadioGroup) findViewById(R.id.radiogroup_metronome);
-		if (metronomeChecked) {
-			metronomeRadioGroup.setVisibility(View.VISIBLE);
-		} else {
-			metronomeRadioGroup.setVisibility(View.GONE);
-		}
 	}
 
 	public void onCheckboxClicked(View view) {
-		metronomeChecked = ((CheckBox) view).isChecked();
+		metronomeActive = ((CheckBox) view).isChecked();
 
 		RadioGroup metronomeRadioGroup = (RadioGroup) findViewById(R.id.radiogroup_metronome);
 
 		switch(view.getId()) {
-			case R.id.radiogroup_metronome:
-				if (metronomeChecked) {
-					metronomeRadioGroup.setVisibility(View.VISIBLE);
-				} else {
-					metronomeRadioGroup.setVisibility(View.GONE);
-				}
-				break;
+		case R.id.checkbox_metronome:
+			if (metronomeActive) {
+				metronomeRadioGroup.setVisibility(View.VISIBLE);
+				metronomeSeekBar.setVisibility(View.VISIBLE);
+				metronomeMinusButton.setVisibility(View.VISIBLE);
+				metronomePlusButton.setVisibility(View.VISIBLE);
+				metronomeText.setVisibility(View.VISIBLE);
+			} else {
+				metronomeRadioGroup.setVisibility(View.GONE);
+				metronomeSeekBar.setVisibility(View.GONE);
+				metronomeMinusButton.setVisibility(View.GONE);
+				metronomePlusButton.setVisibility(View.GONE);
+				metronomeText.setVisibility(View.GONE);
+			}
+			break;
 		}
 	}
 
 	public void onRadioButtonClicked(View view) {
-		// Is the button now checked?
 		boolean checked = ((RadioButton) view).isChecked();
 
-		// Check which radio button was clicked
 		switch(view.getId()) {
-			case R.id.radio_metronome_continue:
-				if (checked)
-					// Pirates are the best
-					break;
-			case R.id.radio_metronome_intervals:
-				if (checked)
-					// Ninjas rule
-					break;
-		}
-	}
-
-
-
-	private void bindPeriodButtons() {
-		// There must be a better way to do this
-		mPeriodButtons = new Button[numPeriods];
-		mPeriodButtons[0] = (Button)findViewById(R.id.button1);
-		mPeriodButtons[1] = (Button)findViewById(R.id.button2);
-		mPeriodButtons[2] = (Button)findViewById(R.id.button3);
-		mPeriodButtons[3] = (Button)findViewById(R.id.button4);
-		mPeriodButtons[4] = (Button)findViewById(R.id.button5);
-		mPeriodButtons[5] = (Button)findViewById(R.id.button6);
-		mPeriodButtons[6] = (Button)findViewById(R.id.button7);
-		mPeriodButtons[7] = (Button)findViewById(R.id.button8);
-		
-		for (int i = 0; i < numPeriods; i++) {
-			mPeriodButtons[i].setOnClickListener(new Button.OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					Button b = (Button)v;
-					mPeriod = Integer.parseInt(b.getText().toString());
-					restart();
-					
-					
-				}
+		case R.id.radio_metronome_continue:
+			if (checked) {
+				metronomeContinue=true;
+				RadioButton metronomeRadioContinue= (RadioButton) findViewById(R.id.radio_metronome_continue);
+				RadioButton metronomeRadioIntervals= (RadioButton) findViewById(R.id.radio_metronome_intervals);
+				metronomeRadioContinue.setChecked(true);
+				metronomeRadioIntervals.setChecked(false);
 			}
-			);
-			
+			break;
+		case R.id.radio_metronome_intervals:
+			if (checked) {
+				metronomeContinue=false;
+				RadioButton metronomeRadioContinue= (RadioButton) findViewById(R.id.radio_metronome_continue);
+				RadioButton metronomeRadioIntervals= (RadioButton) findViewById(R.id.radio_metronome_intervals);
+				metronomeRadioContinue.setChecked(false);
+				metronomeRadioIntervals.setChecked(true);
+			}
+			break;
 		}
 	}
-
 
 	private void restart()
 	{
+		metronomeSeekBar.setProgress(metronomeTempo);
+		metronomeText.setText("" + metronomeTempo);
+		metronomeMinusButton.setClickable(metronomeTempo > 0);
+		metronomePlusButton.setClickable(metronomeTempo < MAX_TEMPO);
 		
-		mSeekBar.setProgress(mTempo);
-		tempoVal.setText("" + mTempo);
-		mPeriodLabel.setText("" + mPeriod);
-		mMinus.setClickable(mTempo > 0);
-		mPlus.setClickable(mTempo < maxTempo);
-		
-		if (mRunning) {
+		if (metronomeRunning) {
 			tp.onStop();
-			tp.onStart(mPeriod, mTempo);
+			tp.onStart(/*mPeriod,*/ metronomeTempo);
 		}
-		
-	
 	}
-	/*
-	protected void onPause()
-	{
-		Log.v("Metronome", "onPause called");
-		super.onPause();
-		if (mRunning)
-			changeState();
-		
-		
-	}*/
-	
+
 	protected void onStop() {
-		//Log.v("Metronome", "onStop");
 		super.onStop();
-		/*
-		if (mRunning) {
-			changeState();
-		}
-		*/
 		SharedPreferences settings = getSharedPreferences(PREFS, 0);
 		SharedPreferences.Editor editor = settings.edit();
-		editor.putInt(KEY_PERIOD, mPeriod);
-		editor.putInt(KEY_TEMPO, mTempo);
+		//editor.putInt(KEY_PERIOD, mPeriod);
+		editor.putInt(KEY_TEMPO, metronomeTempo);
 		editor.commit();
 	}
 
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_metronome, menu);
 		return true;
 	}
 
-	/**
-	 * On selecting action bar icons
-	 * */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Take appropriate action for each action item click
 		switch (item.getItemId()) {
-			case R.id.action_metronome:
-				metronomeSettings();
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
+		case R.id.action_metronome:
+			metronomeSettings();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
 	}
 
@@ -258,12 +222,12 @@ public class Metronome extends ActionBarActivity {
 	}
 
 	private void changeState() {
-     	mRunning = !mRunning;
-    	if (mRunning) {
+     	metronomeRunning = !metronomeRunning;
+    	if (metronomeRunning) {
     		mWakeLock.acquire();
     		
     		mStartStopButton.setText(R.string.stop);
-    		tp.onStart(mPeriod, mTempo);
+    		tp.onStart(/*mPeriod,*/ metronomeTempo);
     	} else {
     		mWakeLock.release();
     		tp.onStop();
@@ -271,25 +235,13 @@ public class Metronome extends ActionBarActivity {
     		mStartStopButton.setText(R.string.start);
     	}
     }
-/*
-    protected void onPause() {
-    	Log.v("Metronome", "onPause");
-    	super.onPause();
-    }
-    protected void onResume() {
-    	Log.v("Metronome", "onResume");
-    	super.onResume();
-    }
-    */
-    
+
     protected void onDestroy() {
-    	//Log.v("Metronome", "onDestroy");
-    	if (mRunning) {
+    	if (metronomeRunning) {
     		changeState();
     	}
     	tp.onDestroy();
     
     	super.onDestroy();
-    
     }
 }
--- app/src/main/java/running/org/running/TickPlayer.java
+++ app/src/main/java/running/org/running/TickPlayer.java
@@ -20,167 +20,60 @@
 package running.org.running;
 
 import android.content.Context;
-import android.media.MediaPlayer;
 import android.os.Handler;
 import android.os.Message;
-import android.os.SystemClock;
-import android.util.Log;
-/*
-public class TickPlayer implements MediaPlayer.OnCompletionListener {
-	
-	MediaPlayer tick;
-	MediaPlayer tock;
-	boolean mRunning = false;
-	int mCount = 0;
-	int mPeriod = 1;
-	private long mTickDuration;
-	private long mStopTimeInFuture;
-	
-	public TickPlayer(Context ctx) {
-		
-		tick = MediaPlayer.create(ctx, R.raw.noises_tick);
-		tock = MediaPlayer.create(ctx, R.raw.noises_tock);
-		
-		
-		tick.setOnCompletionListener(this);
-		tock.setOnCompletionListener(this);
-			
-		tick.setLooping(false);
-		tock.setLooping(false);
-	}
-	
-	public void onStart(int period, int ticksPerSec) {
-		mRunning = true;
-		mPeriod = (period == 0) ? 1 : period;
-		mCount = 0;
-		
-		mTickDuration = 60000 / ticksPerSec;
-		run();
-		Log.i("metronome", "tid: " + Thread.currentThread().getId() + " onStart");
-		
-	}
-	
-	private  void run() {
-		Log.i("metronome", "tid: " + Thread.currentThread().getId() + " run");
-		
-		if (!mRunning)
-			return;
-		
-		mStopTimeInFuture = SystemClock.elapsedRealtime() + mTickDuration;
-		
-		if (++mCount % mPeriod == 0) {
-			mCount = 0;
-		
-			tock.start();
-		} else {
-			
-			tick.start();
-		}
-	}
-	
-	public void onStop() {
-		mRunning = false;
-		mHandler.sendMessage(mHandler.obtainMessage(MSG));
-		
-	}
-	
-	public void onDestroy() {
-		if (tick != null) {
-			tick.release();
-			tick = null;
-		}
-		
-		if (tock != null) {
-			tock.release();
-			tock = null;
-		}
-		
-	}
 
-	@Override
-	public void onCompletion(MediaPlayer mp) {
-		
-		
-		//mp.seekTo(0);
-		
-		long delay = mStopTimeInFuture - SystemClock.elapsedRealtime();
-		Log.i("metronome", "tid: " + Thread.currentThread().getId() + "delay = " + delay);
-		while (delay < 0)
-			delay += mTickDuration;
-		
-		
-		mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG), delay);
-		
-	}
-	
-	private static final int MSG = 1;
-	
-	private Handler mHandler = new Handler() {
-		public void handleMessage(Message m) {
-			run();
-		}
-	};
-
-}
-*/
 public class TickPlayer  {
 	
 	MediaPlayerPool tickPool;
-	MediaPlayerPool tockPool;
-	boolean mRunning = false;
-	int mCount = 0;
-	int mPeriod = 1;
+	//MediaPlayerPool tockPool;
+	boolean metronomeRunning = false;
+	//int mCount = 0;
+	//int mPeriod = 1;
 	private long mTickDuration;
-	//private long mStopTimeInFuture;
-	
+
 	public TickPlayer(Context ctx) {
 		tickPool = new MediaPlayerPool(ctx, 10, R.raw.tock);
-		tockPool = new MediaPlayerPool(ctx, 10, R.raw.tick);
+		//tockPool = new MediaPlayerPool(ctx, 10, R.raw.tick);
 	}
 	
-	public void onStart(int period, int ticksPerSec) {
-		mRunning = true;
-		mPeriod = (period == 0) ? 1 : period;
-		mCount = 0;
+	public void onStart(/*int period,*/ int ticksPerSec) {
+		metronomeRunning = true;
+		//mPeriod = (period == 0) ? 1 : period;
+		//mCount = 0;
 		
 		mTickDuration = 60000 / ticksPerSec;
 		run();
-		//Log.i("metronome", "tid: " + Thread.currentThread().getId() + " onStart");
 	}
 	
 	private  void run() {
-		//Log.i("metronome", "tid: " + Thread.currentThread().getId() + " run");
-		if (!mRunning)
+		if (!metronomeRunning)
 			return;
-		
-		//mStopTimeInFuture = SystemClock.elapsedRealtime() + mTickDuration;
-		
-		if ((mPeriod != 1) && (++mCount % mPeriod == 0)) {
-			mCount = 0;
-			tockPool.start();
-		} else {
-			tickPool.start();
-		}
+
+		//if ((mPeriod != 1) && (++mCount % mPeriod == 0)) {
+		//	mCount = 0;
+		//	tockPool.start();
+		//} else {
+		//	tickPool.start();
+		//}
 		mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG), mTickDuration);
 	}
 	
 	public void onStop() {
-		mRunning = false;
-		mCount = 0;
+		metronomeRunning = false;
+		//mCount = 0;
 		tickPool.stop();
-		tockPool.stop();
+		//tockPool.stop();
 		mHandler.removeMessages(MSG);
-		//mHandler.sendMessage(mHandler.obtainMessage(MSG));
 	}
 	
 	public void onDestroy() {
 		onStop();
 		tickPool.onDestroy();
-		tockPool.onDestroy();
+		//tockPool.onDestroy();
 	}
 
 	private static final int MSG = 1;
-	
 	private Handler mHandler = new Handler() {
 		public void handleMessage(Message m) {
 			run();
